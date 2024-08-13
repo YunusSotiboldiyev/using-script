4bbab24248be9bb150e004a363082e9a96050242
Yunus <152853532+YunusSotiboldiyev@users.noreply.github.com>
2024-08-13 13:09:59 +0500

    Add files via upload
    using script to get information

---
+++ one website/script.js
@@ -0,0 +1,21 @@
+let a = prompt("'111, 223, 341, 555, 441, 638, 690'1tasini tanla");
+if (a===111) {
+    document.write("San mani eng yaqin do'stimsan")
+}
+if (a===223){
+    document.write("Seni doim yoningda bo'laman")
+}
+if (a===341) {
+    document.write("Sen yaxshisan")
+}
+if (a===555) {
+    document.write("Sen menga kerak emassan")
+}
+if (a===441) {
+    document.write("Men seni sevaman")
+}
+if (a===638) {
+    document.write("Sen odam emassan")
+}else{
+    document.write("Jonimga tegib kettin")
+}
